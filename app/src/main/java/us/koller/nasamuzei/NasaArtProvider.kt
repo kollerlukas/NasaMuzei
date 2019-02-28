2f664cc36eb57c776db9135da091473d2d5ace9e
Lukas Koller <lukaskoller6@gmail.com>
2019-02-28 22:34:15 +0100

    inital commit

---
+++ app/src/main/java/us/koller/nasamuzei/NasaArtProvider.kt
@@ -0,0 +1,67 @@
+package us.koller.nasamuzei
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import com.google.android.apps.muzei.api.UserCommand
+import com.google.android.apps.muzei.api.provider.Artwork
+import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
+import com.google.android.apps.muzei.api.provider.ProviderContract
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class NasaArtProvider : MuzeiArtProvider() {
+
+    companion object {
+        private const val COMMAND_ID_VIEW_ARCHIVE = 1
+
+        private const val CURRENT_APOD_URL = "https://apod.nasa.gov/apod/astropix.html"
+        private const val APOD_ARCHIVE_URL = "https://apod.nasa.gov/apod/archivepix.html"
+    }
+
+    private var disposable: Disposable? = null
+
+    override fun onLoadRequested(initial: Boolean) {
+        val context = context ?: return
+
+        val authority = context.getString(R.string.muzei_art_provider_authority)
+        val apiKey = context.getString(R.string.nasa_api_key)
+
+        disposable = NasaService.apod(apiKey, null /* Type: YYYY-MM-DD*/)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { res ->
+                            val providerClient = ProviderContract.getProviderClient(context, authority)
+                            providerClient.addArtwork(Artwork().apply {
+                                token = res.url
+                                title = res.title
+                                byline = res.copyright ?: "Public Domain"
+                                attribution = res.explanation
+                                persistentUri = Uri.parse(res.url)
+                                webUri = Uri.parse(CURRENT_APOD_URL)
+                            })
+                        }
+                )
+                /* error */
+                { err -> Log.d("NasaArtProvider", "apod(), error: " + err.message) }
+    }
+
+    /* add user command to view the archive of all APOD pictures */
+    override fun getCommands(artwork: Artwork): MutableList<UserCommand> {
+        return mutableListOf(UserCommand(COMMAND_ID_VIEW_ARCHIVE, context.getString(R.string.view_archive)))
+    }
+
+    override fun onCommand(artwork: Artwork, id: Int) {
+        val context = context ?: return
+        when (id) {
+            COMMAND_ID_VIEW_ARCHIVE -> {
+                /* open archive website */
+                context.startActivity(
+                        Intent(Intent.ACTION_VIEW, Uri.parse(APOD_ARCHIVE_URL))
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/us/koller/nasamuzei/NasaService.kt
@@ -0,0 +1,56 @@
+package us.koller.nasamuzei
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+internal interface NasaService {
+
+    companion object {
+        private fun createService(): NasaService {
+            /* instantiate service */
+            return Retrofit.Builder()
+                    /* add RxJava */
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    /* add Gson */
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.nasa.gov/planetary/")
+                    .build()
+                    .create(NasaService::class.java)
+        }
+
+        internal fun apod(api_key: String, date: String?): Observable<ApodImage> {
+            return createService().apod(api_key, date)
+        }
+    }
+
+    /* api call to get the current APOD */
+    @GET("apod")
+    fun apod(@Query("api_key") api_key: String, @Query("date") date: String?): Observable<ApodImage>
+
+    /*
+    * ApodImage sample data:
+    * {
+        "date": "2019-02-28",
+        "explanation": "On January 1, New Horizons swooped to within 3,500 kilometers of the Kuiper Belt world known as Ultima Thule. That's about 3 times closer than its July 2015 closest approach to Pluto. The spacecraft's unprecedented feat of navigational precision, supported by data from ground and space-based observing campaigns, was accomplished 6.6 billion kilometers (over 6 light-hours) from planet Earth. Six and a half minutes before closest approach to Ultima Thule it captured the nine frames used in this composite image. The most detailed picture possible of the farthest object ever explored, the image has a resolution of about 33 meters per pixel, revealing intriguing bright surface features and dark shadows near the terminator. A primitive Solar System object, Ultima Thule's two lobes combine to span just 30 kilometers. The larger lobe, referred to as Ultima, is recently understood to be flattened like a fluffy pancake, while the smaller, Thule, has a shape that resembles a dented walnut.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1902/ultima-thule-1-ca06_022219.png",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Sharpest Ultima Thule",
+        "url": "https://apod.nasa.gov/apod/image/1902/ultima-thule-1-ca06_022219_1024.jpg"
+    }
+    * */
+    data class ApodImage(
+            val date: String,
+            val explanation: String,
+            val hdurl: String,
+            val media_type: String,
+            val service_version: String,
+            val title: String,
+            val url: String,
+            val copyright: String?)
+}
+
